4fc44b4fa93a66f107f928233179bb2ef8dea7f6
darkenvy <darkenvy6@gmail.com>
2016-12-24 19:03:39 -0800

    Ported PSMRC to work under unique cookie folders.

--- psmrc.js
+++ psmrc.js
@@ -7,14 +7,13 @@ var singleSprites = require('./sprites-single');
 // var importDirectory = findImportDirectory(); // Declare so find only runs once
 var importDirectory;
 var layout = [
-  // '/export/armor',
-  // '/export/font',
-  // '/export/item',
-  // '/export/misc',
-  // '/export/mob',
-  '/export',
-  '/export/art',
-  '/export/textures'
+  // '/armor',
+  // '/font',
+  // '/item',
+  // '/misc',
+  // '/mob',
+  '/art',
+  '/textures'
 ]
 
 // ----------------- Directory Functions ----------------- //
@@ -48,21 +47,38 @@ function findImportDirectory() {
 }
 
 function findPNG(shortDirectory) {
-  // Returns either the imported texturepack path or the default path
-  return importDirectory.then(dir => {
-    let userDir = './import/' + dir + '/assets/minecraft/textures/' + shortDirectory;
-    let defaultDir = './import/default/assets/minecraft/textures/' + shortDirectory;
+  return new Promise( (resolve, reject) => {
+    let userDir = importDirectory + '-unzip/assets/minecraft/textures/' + shortDirectory;
+    let defaultDir = './uploads/default/assets/minecraft/textures/' + shortDirectory;
     // if (false) {
     if (fs.existsSync(userDir)) {
-      return userDir;
+      resolve(userDir);
     } else if (fs.existsSync(defaultDir)) {
-      return defaultDir;
+      resolve(defaultDir);
     } else {
       console.log("!!!ERROR!!! Report this filename: \"", shortDirectory, "\"");
-      return './import/default/404.png';
+      resolve('./uploads/default/404.png');
     }
   })
+
 }
+
+// function findPNG(shortDirectory) {
+//   // Returns either the imported texturepack path or the default path
+//   return importDirectory.then(dir => {
+//     let userDir = importDirectory + '/import/' + dir + '/assets/minecraft/textures/' + shortDirectory;
+//     let defaultDir = importDirectory + '/import/default/assets/minecraft/textures/' + shortDirectory;
+//     // if (false) {
+//     if (fs.existsSync(userDir)) {
+//       return userDir;
+//     } else if (fs.existsSync(defaultDir)) {
+//       return defaultDir;
+//     } else {
+//       console.log("!!!ERROR!!! Report this filename: \"", shortDirectory, "\"");
+//       return './import/default/404.png';
+//     }
+//   })
+// }
 
 // ----------------- Imagemagick Functions ----------------- //
 
@@ -89,7 +105,7 @@ function moveAndConvert() {
   let apply = function() {
     if (counter < singleSprites.length) {
       findPNG(singleSprites[counter][2]).then(dir => {
-        if (dir == './import/default/404.png') {
+        if (dir == './uploads/default/404.png') {
           console.log('missing ', singleSprites[counter[2]], 'skipping...');
           counter += 1;
           apply();
@@ -99,7 +115,7 @@ function moveAndConvert() {
             .resize(singleSprites[counter][0],singleSprites[counter][1])
             .bitdepth(8)
             .background('transparent')
-            .write('export/' + singleSprites[counter][3], err => {
+            .write(importDirectory + '-create/' + singleSprites[counter][3], err => {
               if (err) {console.log('error writing on ' + counter + '[3]:', err)
               } else {
                 console.log('written ' + singleSprites[counter][3]);
@@ -118,7 +134,7 @@ function saveSheet(spritesheet, filename, size) {
   console.log('ran saver function');
   spritesheet
     .geometry('16x16+0+0').tile(size)
-    .write('export/' + filename, (err) => {
+    .write(importDirectory + '-create/' + filename, (err) => {
       if (!err) {console.log('written', filename)} 
       else {console.log(err);}
     })
@@ -127,13 +143,13 @@ function saveSheet(spritesheet, filename, size) {
     // Only terrain.png has MipMapLevels
     spritesheet
       .geometry('8x8+0+0').tile(size)
-      .write('export/terrainMipMapLevel2.png', (err) => {
+      .write(importDirectory + '-create/terrainMipMapLevel2.png', (err) => {
         if (!err) {console.log('written terrainMipMapLevel2')} 
       })
 
     spritesheet
       .geometry('4x4+0+0').tile(size)
-      .write('export/terrainMipMapLevel3.png', (err) => {
+      .write(importDirectory + '-create/terrainMipMapLevel3.png', (err) => {
         if (!err) {console.log('written terrainMipMapLevel3')} 
       })  
   }
@@ -152,8 +168,8 @@ module.exports = function(cookie) {
     
     // Organize an output folder
     // Create specific user's cookie folder first
-    importDirectory = 'uploads/' + cookie + '-create';
-    fs.mkdir(importDirectory, (err) => {
+    importDirectory = 'uploads/' + cookie;
+    fs.mkdir(importDirectory + '-create', (err) => {
       if (err) {console.log('err', err);}
 
       // Create layout inside user's cookie folder
@@ -167,11 +183,11 @@ module.exports = function(cookie) {
 
       var blockSpritesheet = gm(256,512).bitdepth(8).background('transparent');
       var itemSpritesheet = gm(256,256).bitdepth(8).background('transparent');
-      // montageEach(blockSpritesheet, blockSprites, '16x32', 'blocks/', 'terrain.png');
-      // montageEach(itemSpritesheet, itemSprites, '16x16', 'items/', 'items.png');
+      montageEach(blockSpritesheet, blockSprites, '16x32', 'blocks/', 'terrain.png');
+      montageEach(itemSpritesheet, itemSprites, '16x16', 'items/', 'items.png');
 
       // Copy Destroy Stages 0-9
-      // moveAndConvert();
+      moveAndConvert();
 
 
 
